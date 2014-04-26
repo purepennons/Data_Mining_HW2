470cdf97446d79a7a9c4f10f8287c6e68b5f83ce
purepennons <y80811@gmail.com>
2014-04-27 00:56:23 +0800

    基本功能完成，但尚未驗證。

--- ROM/ROM.js
+++ ROM/ROM.js
@@ -30,23 +30,51 @@ var euclideanDistance = function(p1, p2) {  //Euclidean distance without sqrt
 	return Math.pow((p1-p2), 2);
 }
 
+var compareNum = function(num1, num2) {
+	if(num1 > num2){
+		return 1;
+	}
+	if(num1 == num2){
+		return 0;
+	}
+	if(num1 < num2){
+		return -1;
+	}
+}
+
+var getMiniNumFromMatrix = function(matrix){	//回傳矩陣內最小值及其座標
+	var minNum = matrix[0][0];
+	var minX = 0, minY = 0;
+	for(var x in matrix){
+		for(var y in matrix[0]){
+			if(compareNum(minNum, matrix[x][y]) == 1){
+				minNum = matrix[x][y];
+				minX = x;
+				minY = y;
+			}
+		}
+	}
+	return [minNum, minX, minY];
+}
+
 function ROM(){
 	this.numOfInput = 0;
 	this.numOfOutputX = 0;
 	this.numOfOutputY = 0;
 	this.X = null;
 	this.W = null;
+	this.dW = null;
 	this.node = null;
 	this.Y = null;
+	this.R = 0;
 
 	this.init = function(numOfInput, numOfOutputX, numOfOutputY){  //初始化參數與陣列
 		this.numOfInput = numOfInput;
 		this.numOfOutputX = numOfOutputX;
 		this.numOfOutputY = numOfOutputY;
 		this.X = createArray(numOfInput, 0);
-		this.Y = createMatrix(numOfOutputX, numOfOutputY, 0);
-		this.node = createMatrix(numOfOutputX, numOfOutputY, 0);
 		this.W = createArray(numOfInput, 0);
+		this.R = numOfOutputX;  //初始相鄰半徑為numOfOutputX
 		for(var i in this.W){
 			this.W[i] = createMatrix(numOfOutputX, numOfOutputY, 0);
 		}
@@ -62,7 +90,13 @@ function ROM(){
 
 	/************ main function *****************/
 
-	this.update = function(input) {
+	this.update = function(input, learningRate, radiusRate) {
+		var minNum = 0, minX = 0, minY = 0;
+		var error = 0;
+		this.node = createMatrix(this.numOfOutputX, this.numOfOutputY, 0);
+		var neighborhood = createMatrix(this.numOfOutputX, this.numOfOutputY, 0);
+		var r = createMatrix(this.numOfOutputX, this.numOfOutputY, 0);
+		this.Y = createMatrix(this.numOfOutputX, this.numOfOutputY, 0);
 		for(var data in input){
 			this.X[data] = input[data];
 		}
@@ -75,29 +109,56 @@ function ROM(){
 				}
 			}
 		}
-		console.log(this.node);
+		var tempArray = getMiniNumFromMatrix(this.node);
+		minNum = parseInt(tempArray[0]);
+		minX = parseInt(tempArray[1]);
+		minY = parseInt(tempArray[2]);
+		this.Y[minX][minY] = 1;		//if j=j*, k=k*, node[j][k] = 1, else node[j][k] = 0
+		error = Math.sqrt(this.node[minX][minY]);
 
-	}
+		//計算neighborhood與dW，並且更新this.W
+		this.dW = createArray(this.numOfInput, 0);
+		for(var i in this.dW){
+			this.dW[i] = createMatrix(this.numOfOutputX, this.numOfOutputY, 0);
+		}
 
-	this.train = function(inputData, maxTrainTimes, learningRate, radius) {
-		//for(var iteration=0;iteration<maxTrainTimes;iteration++){
-			for(var i in inputData){
-				this.update(inputData[i]);
+		for(var j=0;j<this.numOfOutputX;j++){
+			for(var k=0;k<this.numOfOutputY;k++){
+				r[j][k] = Math.sqrt(euclideanDistance(j, minX) + euclideanDistance(k, minY));
+				neighborhood[j][k] = Math.exp((-r[j][k])/this.R);
+				for(var i=0;i<this.numOfInput;i++){
+					this.dW[i][j][k] = learningRate * (this.X[i] - this.W[i][j][k]) * neighborhood[j][k];
+					this.W[i][j][k] += this.dW[i][j][k];
+				}
 			}
-		//}
-
+		}
+		return error;
 	}
 
-	this.recall = function() {
+	this.train = function(inputData, maxTrainTimes, learningRate, radiusRate) {
+		for(var iteration=0;iteration<maxTrainTimes;iteration++){
+			var error = 0;
+			for(var i in inputData){
+				error += this.update(inputData[i], learningRate, radiusRate);
+			}
+			if(iteration%(iteration*0.05) ==0){
+				console.log(error);
+				learningRate *= 0.9;
+				radiusRate *= 0.9;	
+			}
+		}
 
 	}
-
 }
+
+
+
 var inputData = [
-					[19,3,5],
-					[191,53,2],
-					[159,43,15]
+					[19,334,5],
+					[191,53,432],
+					[19,334,5],
+					[15229,43,15]
 					];
 var rom = new ROM();
-rom.init(3, 5, 5);
-rom.train(inputData, 1000, 0.3, 0.3);
+rom.init(3, 10, 10);
+rom.train(inputData, 100000, 1.0, 1.0);
